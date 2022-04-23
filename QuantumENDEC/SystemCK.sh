0a9098f76f6a9dce0dc12c65e2d0d2e67b90d8fd
ApatheticDELL <92345385+ApatheticDELL@users.noreply.github.com>
2022-04-22 20:06:51 -0400

    what

--- QuantumENDEC/SystemCK.sh
+++ QuantumENDEC/SystemCK.sh
@@ -1,9 +1,9 @@
 echo "System check!"
-id=879106404160507996
+id=
 message="AKR24 system check"
 	
 curl -v \
--H "Authorization: Bot ODk5MTAyMDcyOTc5NDc2NDkx.YWt4Dg.Lk7JGYGilcNSsXi2IqGQbSPn_rk" \
+-H "Authorization: Bot" \
 -H "User-Agent: AKQ" \
 -H "Content-Type: application/json" \
 -X POST \
